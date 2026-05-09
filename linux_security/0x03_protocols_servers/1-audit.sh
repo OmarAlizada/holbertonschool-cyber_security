102b784f60b0f229a9db03c976c110a09079571b
OmarAlizada <omer.alizade57@gmail.com>
2026-05-09 19:43:06 +0000

    Audit SSH

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-# SSH konfiqurasiya fayllarını audit edən skript
-
-CONFIG_FILE="/etc/ssh/sshd_config"
-INCLUDE_DIR="/etc/ssh/sshd_config.d/*.conf"
-
-# Əsas faylı və include edilən faylları birlikdə yoxlayırıq
-grep -v '^#' $CONFIG_FILE $INCLUDE_DIR 2>/dev/null | grep -E "KbdInteractiveAuthentication|UsePAM|X11Forwarding|PrintMotd|AcceptEnv|Subsystem|PasswordAuthentication|PermitRootLogin|AuthorizedKeysFile|TCPKeepAlive"
+grep -Ev "^(Port|PermitRootLogin|PasswordAuthentication|AllowUsers|AllowGroups)" /etc/ssh/sshd_config; sudo ss -tulpn | grep ssh
