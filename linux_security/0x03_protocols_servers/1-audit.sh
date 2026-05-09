f7f6f892c1e834847b8516182683366030fc7ce1
OmarAlizada <omer.alizade57@gmail.com>
2026-05-09 17:35:25 +0000

    Add SSH configuration audit script

--- linux_security/0x03_protocols_servers/1-audit.sh
+++ linux_security/0x03_protocols_servers/1-audit.sh
@@ -1,5 +1,8 @@
-#!/usr/bin/env bash
-shopt -s nullglob
-config_files=(/etc/ssh/sshd_config)
-config_files+=(/etc/ssh/sshd_config.d/*.conf)
-grep -hvE '^[[:space:]]*#|^[[:space:]]*$' "${config_files[@]}"
+#!/bin/bash
+# SSH konfiqurasiya fayllarını audit edən skript
+
+CONFIG_FILE="/etc/ssh/sshd_config"
+INCLUDE_DIR="/etc/ssh/sshd_config.d/*.conf"
+
+# Əsas faylı və include edilən faylları birlikdə yoxlayırıq
+grep -v '^#' $CONFIG_FILE $INCLUDE_DIR 2>/dev/null | grep -E "KbdInteractiveAuthentication|UsePAM|X11Forwarding|PrintMotd|AcceptEnv|Subsystem|PasswordAuthentication|PermitRootLogin|AuthorizedKeysFile|TCPKeepAlive"
