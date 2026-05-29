4a71860038183971b33e31d3d3719b1621af71b5
OmarAlizada <omer.alizade57@gmail.com>
2026-05-29 19:47:27 +0000

    Fix format for check 2

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-# Wordlist ilə parolları qırırıq
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1"
+# Həmişə eyni nəticəni verməsi üçün əvvəlki potesial keşləri təmizləyirik
+john --show "$1" > /dev/null 2>&1
 
-# Sırf parolları təmiz şəkildə çıxarıb 4-password.txt faylına yazırıq
-john --show "$1" | awk -F: '/^[a-f0-9]/ {print $2}' > 4-password.txt
+# Wordlist ilə parolları qırırıq
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" > /dev/null 2>&1
+
+# Parolları çıxarırıq: ":" simvolundan sonrakı hissəni götürürük, boş sətirləri və John-un statistikalarını silirik
+john --show "$1" | grep -E '^[a-f0-9]+:' | cut -d':' -f2 | grep -v '^$' > 4-password.txt
