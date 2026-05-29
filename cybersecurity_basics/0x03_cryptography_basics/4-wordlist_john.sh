d2493d7b7b2de7c51a6de0bfbea9b5d1fcc055bf
OmarAlizada <omer.alizade57@gmail.com>
2026-05-29 19:46:50 +0000

    Fix: John the ripper wordlist task

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# Wordlist ilə parolları qırırıq
 john --wordlist=/usr/share/wordlists/rockyou.txt "$1"
+
+# Sırf parolları təmiz şəkildə çıxarıb 4-password.txt faylına yazırıq
+john --show "$1" | awk -F: '/^[a-f0-9]/ {print $2}' > 4-password.txt
