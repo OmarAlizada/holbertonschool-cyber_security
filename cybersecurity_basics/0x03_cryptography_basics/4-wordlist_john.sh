57eebdf58da021813b36d8e678829d7a7258f2bf
OmarAlizada <omer.alizade57@gmail.com>
2026-05-29 19:48:10 +0000

    fix: master sync for task 2 and task 4 checks

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-# Həmişə eyni nəticəni verməsi üçün əvvəlki potesial keşləri təmizləyirik
-john --show "$1" > /dev/null 2>&1
-
-# Wordlist ilə parolları qırırıq
-john --wordlist=/usr/share/wordlists/rockyou.txt "$1" > /dev/null 2>&1
-
-# Parolları çıxarırıq: ":" simvolundan sonrakı hissəni götürürük, boş sətirləri və John-un statistikalarını silirik
-john --show "$1" | grep -E '^[a-f0-9]+:' | cut -d':' -f2 | grep -v '^$' > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1"
