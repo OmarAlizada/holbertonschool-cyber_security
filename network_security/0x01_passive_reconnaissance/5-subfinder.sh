6d086d484f0a857e9385b917bf1d43d2c99eacf4
OmarAlizada <omer.alizade57@gmail.com>
2026-06-14 17:28:03 +0000

    add 5-subfinder.sh with Host,IP format

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-# Subdomain-ləri ekrana çıxarır və eyni zamanda dövrə salır
-subfinder -d "$1" -silent | tee /dev/tty | while read -r sub; do
-    # Hər subdomain üçün IP adresini tapır
-    ip=$(dig +short "$sub" | tail -n1)
-    # Əgər IP tapılarsa, fayla "Host,IP" formatında yazır
-    if [ -not -z "$ip" ]; then
-        echo "$sub,$ip" >> "$1.txt"
-    fi
-done
+subfinder -d $1 -silent -oI -nW -o $1.txt
