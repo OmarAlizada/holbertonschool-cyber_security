13b2450ed5e9ad72fcede8f77f04dce379787691
OmarAlizada <omer.alizade57@gmail.com>
2026-06-14 16:27:13 +0000

    fixed 0-whois.sh

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F ": " '/^(Registrant|Admin|Tech)/ {k=$1; sub(/^[ \t]+/, "", k); sub(/[ \t]+$/, "", k); v=$2; sub(/^[ \t]+/, "", v); sub(/[ \t]+$/, "", v); if (k ~ /Ext$/) k=k ":"; if (k ~ /Street$/ && v != "") v=v " "; lines[++count]=k "," v} END {for (i=1; i<=count; i++) {if (i==count) printf "%s", lines[i]; else print lines[i]}}' > "$1.csv"
+whois $1|awk -F': *' '/^(Registrant|Admin|Tech)/{k=$1;v=$2;if(k~/ Ext$/)k=k":";print (v==""?k", ":k", "v)}' > $1.csv
